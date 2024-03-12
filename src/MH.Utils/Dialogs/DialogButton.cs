8d8514e394217ccc9fc001dc6afa088bbbcdd905
martin-holy <martin.holy@hotmail.com>
2024-03-12 22:46:19 +0100

    [C] DialogButton move MH.Utils.Dialogs => MH.UI.Controls

--- src/MH.Utils/Dialogs/DialogButton.cs
+++
@@ -1,9 +0,0 @@
-﻿using MH.Utils.BaseClasses;
-
-namespace MH.Utils.Dialogs;
-
-public class DialogButton(RelayCommand command, bool isDefault = false, bool isCancel = false) : ObservableObject {
-  public bool IsDefault { get; } = isDefault;
-  public bool IsCancel { get; } = isCancel;
-  public RelayCommand Command { get; } = command;
-}

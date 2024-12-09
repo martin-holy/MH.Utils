86d650430cfffe54eeb1bf4bc7965dcb352b1d5d
martin-holy <martin.holy@hotmail.com>
2024-12-09 11:44:16 +0100

    [N] StringExtensionsTests: ToDouble

---
+++ tests/MH.Utils.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using MH.Utils.Extensions;
+using System.Globalization;
+
+namespace MH.Utils.Tests.Extensions;
+
+[TestClass]
+public class StringExtensionsTests {
+  [TestMethod]
+  public void ToDouble_NullOrEmptyValue_ReturnsNull() {
+    string? value = null;
+    IFormatProvider provider = CultureInfo.InvariantCulture;
+
+    var result = value.ToDouble(provider);
+
+    Assert.AreEqual(null, result);
+  }
+
+  [TestMethod]
+  public void ToDouble_ValidValue_ReturnsDouble() {
+    var value = "123.45";
+    IFormatProvider provider = CultureInfo.InvariantCulture;
+
+    var result = value.ToDouble(provider);
+
+    Assert.AreEqual(123.45, result);
+  }
+}

185d5eff1070d45b26c4b61eca102be615a29751
martin-holy <martin.holy@hotmail.com>
2024-12-09 11:44:39 +0100

    [N] DoubleExtensionsTests: ToString

---
+++ tests/MH.Utils.Tests/Extensions/DoubleExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using MH.Utils.Extensions;
+using System.Globalization;
+
+namespace MH.Utils.Tests.Extensions;
+
+[TestClass]
+public class DoubleExtensionsTests {
+  [TestMethod]
+  public void ToString_NullValue_ReturnsEmptyString() {
+    double? value = null;
+    IFormatProvider provider = CultureInfo.InvariantCulture;
+
+    var result = value.ToString(provider);
+
+    Assert.AreEqual(string.Empty, result);
+  }
+
+  [TestMethod]
+  public void ToString_ValidValue_ReturnsFormattedString() {
+    double? value = 123.45;
+    IFormatProvider provider = CultureInfo.InvariantCulture;
+
+    var result = value.ToString(provider);
+
+    Assert.AreEqual("123.45", result);
+  }
+}

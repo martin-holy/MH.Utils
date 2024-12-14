9d4e667fab9bce5b1dd57c4ecdc1f9d498352eab
martin-holy <martin.holy@hotmail.com>
2024-12-14 15:24:48 +0100

    [C] RoutedEventArgs: code style

--- src/MH.Utils/EventsArgs/RoutedEventArgs.cs
+++ src/MH.Utils/EventsArgs/RoutedEventArgs.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace MH.Utils.EventsArgs {
-  public class RoutedEventArgs : EventArgs {
-    public object? OriginalSource { get; set; }
-    public object? DataContext { get; set; }
-    public bool IsCtrlOn { get; set; }
-    public bool IsAltOn { get; set; }
-    public bool IsShiftOn { get; set; }
-    public bool IsSourceDesired { get; set; }
-  }
+namespace MH.Utils.EventsArgs;
+
+public class RoutedEventArgs : EventArgs {
+  public object? OriginalSource { get; set; }
+  public object? DataContext { get; set; }
+  public bool IsCtrlOn { get; set; }
+  public bool IsAltOn { get; set; }
+  public bool IsShiftOn { get; set; }
+  public bool IsSourceDesired { get; set; }
 }

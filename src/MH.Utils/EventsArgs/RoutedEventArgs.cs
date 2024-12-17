beb2164bb97faee6f54134421e42519109f63076
martin-holy <martin.holy@hotmail.com>
2024-12-17 09:53:54 +0100

    [U] RoutedEventArgs: IsCtrl|Alt|ShiftOn props removed

--- src/MH.Utils/EventsArgs/RoutedEventArgs.cs
+++ src/MH.Utils/EventsArgs/RoutedEventArgs.cs
@@ -5,8 +5,4 @@ namespace MH.Utils.EventsArgs;
 public class RoutedEventArgs : EventArgs {
   public object? OriginalSource { get; set; }
   public object? DataContext { get; set; }
-  public bool IsCtrlOn { get; set; }
-  public bool IsAltOn { get; set; }
-  public bool IsShiftOn { get; set; }
-  public bool IsSourceDesired { get; set; }
 }

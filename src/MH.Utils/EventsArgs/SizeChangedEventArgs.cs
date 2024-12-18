620d6d4f5c4501762f0a10080f097d990389ae32
martin-holy <martin.holy@hotmail.com>
2024-12-18 11:21:59 +0100

    [N] SizeChangedEventArgs

---
+++ src/MH.Utils/EventsArgs/SizeChangedEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using MH.Utils.Types;
+
+namespace MH.Utils.EventsArgs;
+
+public class SizeChangedEventArgs(SizeD previousSize, SizeD newSize, bool widthChanged, bool heightChanged) : RoutedEventArgs {
+  public SizeD PreviousSize { get; } = previousSize;
+  public SizeD NewSize { get; } = newSize;
+  public bool WidthChanged { get; } = widthChanged;
+  public bool HeightChanged { get; } = heightChanged;
+}

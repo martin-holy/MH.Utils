c7a797e3c03bce6fc7b716c03bb3901f1ec9108e
martin-holy <martin.holy@hotmail.com>
2024-12-18 11:21:23 +0100

    [N] ThicknessD struct

---
+++ src/MH.Utils/Types/ThicknessD.cs
@@ -0,0 +1,11 @@
+﻿namespace MH.Utils.Types;
+
+public struct ThicknessD(double left, double top, double right, double bottom) {
+  public double Left { get; set; } = left;
+  public double Top { get; set; } = top;
+  public double Right { get; set; } = right;
+  public double Bottom { get; set; } = bottom;
+
+  public ThicknessD(double uniformLength) :
+    this(uniformLength, uniformLength, uniformLength, uniformLength) { }
+}

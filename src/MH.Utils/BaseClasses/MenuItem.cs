3402611f8a772d8ea96220a2ed44e0fb3f4d400a
martin-holy <martin.holy@hotmail.com>
2025-06-09 15:32:56 +0200

    MenuItem

---
+++ src/MH.Utils/BaseClasses/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+
+namespace MH.Utils.BaseClasses;
+
+public class MenuItem : TreeItem {
+  public ICommand? Command { get; set; }
+  public string? InputGestureText { get; set; }
+
+  public MenuItem(string? icon, string name) : base(icon, name) { }
+
+  public MenuItem(ICommand command) {
+    Command = command;
+  }
+
+  public void Add(MenuItem menuItem) {
+    Items.Add(menuItem);
+  }
+}

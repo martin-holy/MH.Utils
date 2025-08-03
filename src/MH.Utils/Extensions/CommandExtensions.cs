f6d6a027c3cc511f5b563af1783847cc0bd3bd6d
martin-holy <martin.holy@hotmail.com>
2025-08-03 20:46:34 +0200

    [N] CommandExtensions: ExecuteIfCan

---
+++ src/MH.Utils/Extensions/CommandExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Input;
+
+namespace MH.Utils.Extensions;
+
+public static class CommandExtensions {
+  public static void ExecuteIfCan(this ICommand command, object? parameter) {
+    if (command.CanExecute(parameter)) command.Execute(parameter);
+  }
+}

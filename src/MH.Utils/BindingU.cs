2eacc510977c8657b757437d1466d7b6f892a127
martin-holy <martin.holy@hotmail.com>
2025-10-02 12:49:16 +0200

    [C] BindingU

--- src/MH.Utils/BindingU.cs
+++ src/MH.Utils/BindingU.cs
@@ -5,13 +5,12 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
-using System.Windows.Input;
 
 namespace MH.Utils;
 
 public static class BindingU {
-  private static readonly Dictionary<(INotifyPropertyChanged, string), PropertySubscription> _propertySubs = [];
-  private static readonly Dictionary<INotifyCollectionChanged, CollectionSubscription> _collectionSubs = [];
+  private static readonly ConditionalWeakTable<INotifyPropertyChanged, PropertySubscriptionTable> _propertySubs = new();
+  private static readonly ConditionalWeakTable<INotifyCollectionChanged, CollectionSubscriptionTable> _collectionSubs = new();
 
   public static IDisposable Bind<TTarget, TSource, TProp>(
     this TTarget target,
@@ -26,23 +25,21 @@ public static class BindingU {
       throw new ArgumentException("Expression must be a property access", nameof(propertyExpression));
 
     var propertyName = m.Member.Name;
-    var getter = GetterCache.GetGetter(typeof(TSource), propertyName);
+    var getter = GetterCache.GetGetter<TSource, TProp>(propertyName);
     var weakTarget = new WeakReference<TTarget>(target);
 
-    if (!_propertySubs.TryGetValue((source, propertyName), out var sub)) {
-      sub = new PropertySubscription(source, propertyName, getter);
-      _propertySubs[(source, propertyName)] = sub;
-    }
+    var table = _propertySubs.GetOrCreateValue(source);
+    var sub = table.GetOrAdd(source, propertyName, getter);
 
-    void handler(object? valueObj) {
+    void handler(TProp value) {
       if (weakTarget.TryGetTarget(out var t))
-        onChange(t, (TProp)valueObj!);
+        onChange(t, value);
       else
         sub.RemoveHandler(handler);
     }
 
     if (invokeInitOnChange)
-      onChange(target, (TProp)getter(source)!);
+      onChange(target, getter(source));
 
     return sub.AddHandler(handler);
   }
@@ -67,10 +64,8 @@ public static class BindingU {
 
     var weakTarget = new WeakReference<TTarget>(target);
 
-    if (!_collectionSubs.TryGetValue(source, out var sub)) {
-      sub = new CollectionSubscription(source);
-      _collectionSubs[source] = sub;
-    }
+    var table = _collectionSubs.GetOrCreateValue(source);
+    var sub = table.GetOrAdd(source);
 
     void handler(object? s, NotifyCollectionChangedEventArgs e) {
       if (weakTarget.TryGetTarget(out var t))
@@ -84,16 +79,18 @@ public static class BindingU {
     return sub.AddHandler(handler);
   }
 
-  private class PropertySubscription {
-    private readonly INotifyPropertyChanged _source;
+  private class PropertySubscription<TSource, TProp> where TSource : class, INotifyPropertyChanged {
+    private readonly TSource _source;
     private readonly string _propertyName;
-    private readonly Func<object, object?> _getter;
-    private readonly List<Action<object?>> _handlers = [];
+    private readonly Func<TSource, TProp> _getter;
+    private readonly PropertySubscriptionTable _table;
+    private readonly List<Action<TProp>> _handlers = [];
 
-    public PropertySubscription(INotifyPropertyChanged source, string propertyName, Func<object, object?> getter) {
+    public PropertySubscription(TSource source, string propertyName, Func<TSource, TProp> getter, PropertySubscriptionTable table) {
       _source = source;
       _propertyName = propertyName;
       _getter = getter;
+      _table = table;
       _source.PropertyChanged += _onChanged;
     }
 
@@ -105,26 +102,28 @@ public static class BindingU {
         handler(value);
     }
 
-    public Subscription AddHandler(Action<object?> handler) {
+    public IDisposable AddHandler(Action<TProp> handler) {
       _handlers.Add(handler);
-      return new(() => RemoveHandler(handler));
+      return new Subscription(() => RemoveHandler(handler));
     }
 
-    public void RemoveHandler(Action<object?> handler) {
+    public void RemoveHandler(Action<TProp> handler) {
       _handlers.Remove(handler);
       if (_handlers.Count == 0) {
         _source.PropertyChanged -= _onChanged;
-        _propertySubs.Remove((_source, _propertyName));
+        _table.Remove(_propertyName);
       }
     }
   }
 
   private class CollectionSubscription {
     private readonly INotifyCollectionChanged _source;
+    private readonly CollectionSubscriptionTable _table;
     private readonly List<NotifyCollectionChangedEventHandler> _handlers = [];
 
-    public CollectionSubscription(INotifyCollectionChanged source) {
+    public CollectionSubscription(INotifyCollectionChanged source, CollectionSubscriptionTable table) {
       _source = source;
+      _table = table;
       _source.CollectionChanged += _onChanged;
     }
 
@@ -133,34 +132,60 @@ public static class BindingU {
         handler(sender, e);
     }
 
-    public Subscription AddHandler(NotifyCollectionChangedEventHandler handler) {
+    public IDisposable AddHandler(NotifyCollectionChangedEventHandler handler) {
       _handlers.Add(handler);
-      return new(() => RemoveHandler(handler));
+      return new Subscription(() => RemoveHandler(handler));
     }
 
     public void RemoveHandler(NotifyCollectionChangedEventHandler handler) {
       _handlers.Remove(handler);
       if (_handlers.Count == 0) {
         _source.CollectionChanged -= _onChanged;
-        _collectionSubs.Remove(_source);
+        _table.Clear();
       }
     }
   }
 
+  private class PropertySubscriptionTable {
+    private readonly Dictionary<string, object> _subs = new();
+
+    public PropertySubscription<TSource, TProp> GetOrAdd<TSource, TProp>(
+      TSource source, string propertyName, Func<TSource, TProp> getter)
+      where TSource : class, INotifyPropertyChanged {
+
+      if (_subs.TryGetValue(propertyName, out var existing))
+        return (PropertySubscription<TSource, TProp>)existing;
+
+      var sub = new PropertySubscription<TSource, TProp>(source, propertyName, getter, this);
+      _subs[propertyName] = sub;
+      return sub;
+    }
+
+    public void Remove(string propertyName) => _subs.Remove(propertyName);
+  }
+
+  private class CollectionSubscriptionTable {
+    private CollectionSubscription? _sub;
+
+    public CollectionSubscription GetOrAdd(INotifyCollectionChanged source) =>
+      _sub ??= new CollectionSubscription(source, this);
+
+    public void Clear() => _sub = null;
+  }
+
   private static class GetterCache {
-    private static readonly Dictionary<(Type, string), Func<object, object?>> _cache = [];
+    private static readonly Dictionary<(Type, string), Delegate> _cache = [];
 
-    public static Func<object, object?> GetGetter(Type sourceType, string propertyName) {
-      var key = (sourceType, propertyName);
-      if (_cache.TryGetValue(key, out var existing)) return existing;
+    public static Func<TSource, TProp> GetGetter<TSource, TProp>(string propertyName) {
+      var key = (typeof(TSource), propertyName);
+      if (_cache.TryGetValue(key, out var existing))
+        return (Func<TSource, TProp>)existing;
 
-      // build expression: (object src) => (object) ((TSource)src).Property
-      var srcParam = Expression.Parameter(typeof(object), "src");
-      var converted = Expression.Convert(srcParam, sourceType);
-      var prop = Expression.Property(converted, propertyName);
-      var boxed = Expression.Convert(prop, typeof(object));
-      var lambda = Expression.Lambda<Func<object, object?>>(boxed, srcParam);
+      var srcParam = Expression.Parameter(typeof(TSource), "src");
+      var prop = Expression.Property(srcParam, propertyName);
+      var lambda = Expression.Lambda<Func<TSource, TProp>>(prop, srcParam);
       var compiled = lambda.Compile();
+
       _cache[key] = compiled;
       return compiled;
     }

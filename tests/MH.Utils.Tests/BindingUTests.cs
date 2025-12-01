60085281e36d93800251924d4fa11999a07ba841
martin-holy <martin.holy@hotmail.com>
2025-12-01 16:11:51 +0100

    [U] BindingUTests

--- tests/MH.Utils.Tests/BindingUTests.cs
+++ tests/MH.Utils.Tests/BindingUTests.cs
@@ -6,12 +6,12 @@ namespace MH.Utils.Tests;
 [TestClass]
 public class BindingUTests {
 
-  class Test : ObservableObject {
+  public class Test : ObservableObject {
     private TestData? _data;
     public TestData? Data { get { return _data; } set { _data = value; OnPropertyChanged(); } }
   }
 
-  class TestData : ObservableObject {
+  public class TestData : ObservableObject {
     private string? _name;
     private ObservableCollection<string>? _strings;
 
@@ -23,7 +23,7 @@ public class BindingUTests {
   public void P1_RootProperty_InitialAndChange() {
     var list = new List<string?>();
     var o = new TestData { Name = "A" };
-    o.Bind(o, x => x.Name, (t, p) =>  list.Add(p));
+    o.Bind(o, nameof(o.Name), x => x.Name, (t, p) => list.Add(p));
     o.Name = "B";
     CollectionAssert.AreEqual(new[] { "A", "B" }, list);
   }
@@ -32,7 +32,7 @@ public class BindingUTests {
   public void P2_RootProperty_NullToValue() {
     var list = new List<string?>();
     var o = new TestData { Name = null };
-    o.Bind(o, x => x.Name, (t, p) => list.Add(p));
+    o.Bind(o, nameof(o.Name), x => x.Name, (t, p) => list.Add(p));
     o.Name = "X";
     CollectionAssert.AreEqual(new[] { null, "X" }, list);
   }
@@ -41,7 +41,7 @@ public class BindingUTests {
   public void P3_RootProperty_ValueToNull() {
     var list = new List<string?>();
     var o = new TestData { Name = "A" };
-    o.Bind(o, x => x.Name, (t, p) => list.Add(p));
+    o.Bind(o, nameof(o.Name), x => x.Name, (t, p) => list.Add(p));
     o.Name = null;
     CollectionAssert.AreEqual(new[] { "A", null }, list);
   }
@@ -50,7 +50,11 @@ public class BindingUTests {
   public void P4_NestedProperty_InitialAndChange() {
     var list = new List<string?>();
     var o = new Test { Data = new TestData { Name = "A" } };
-    o.BindNested(o, x => x.Data.Name, (t, p) => list.Add(p));
+    o.Bind<Test, Test, string>(
+      o,
+      [nameof(Test.Data), nameof(TestData.Name)],
+      [s => (s as Test)?.Data, s => (s as TestData)?.Name],
+      (t, p) => list.Add(p));
     o.Data.Name = "B";
     CollectionAssert.AreEqual(new[] { "A", "B" }, list);
   }
@@ -59,7 +63,11 @@ public class BindingUTests {
   public void P5_NestedProperty_NullData_ThenSetData() {
     var list = new List<string?>();
     var o = new Test { Data = null };
-    o.BindNested(o, x => x.Data.Name, (t, p) => list.Add(p));
+    o.Bind<Test, Test, string>(
+      o,
+      [nameof(Test.Data), nameof(TestData.Name)],
+      [s => (s as Test)?.Data, s => (s as TestData)?.Name],
+      (t, p) => list.Add(p));
     o.Data = new TestData { Name = "X" };
     CollectionAssert.AreEqual(new[] { null, "X" }, list);
   }
@@ -68,16 +76,24 @@ public class BindingUTests {
   public void C1_RootCollection_NullToNew() {
     var list = new List<string?>();
     var o = new TestData { Strings = null };    
-    o.BindCollectionNested(o, x => x.Strings, (t, e) => { list.Add(t.Strings == null ? null : string.Join(", ", t.Strings)); });
-    o.Strings = new ObservableCollection<string> { "A", "B" };
+    o.Bind(
+      o,
+      nameof(o.Strings),
+      s => s.Strings,
+      (t, c, e) => { list.Add(c == null ? null : string.Join(", ", c)); });
+    o.Strings = ["A", "B"];
     CollectionAssert.AreEqual(new List<string?>() { "A, B" }, list);
   }
 
   [TestMethod]
   public void C2_RootCollection_AddRemove() {
     var list = new List<string?>();
-    var o = new TestData { Strings = new ObservableCollection<string>() { "X" } };
-    o.BindCollectionNested(o, x => x.Strings, (t, e) => { list.Add(t.Strings == null ? null : string.Join(", ", t.Strings)); });
+    var o = new TestData { Strings = ["X"] };
+    o.Bind(
+      o,
+      nameof(o.Strings),
+      s => s.Strings,
+      (t, c, e) => { list.Add(c == null ? null : string.Join(", ", c)); });
     o.Strings.Add("A");
     o.Strings.Add("B");
     o.Strings.Remove("A");
@@ -88,17 +104,25 @@ public class BindingUTests {
   public void C3_NestedCollection_NullToNew() {
     var list = new List<string?>();
     var o = new Test { Data = new TestData { Strings = null } };
-    o.BindCollectionNested(o, x => x.Data.Strings, (t, e) => { list.Add(t.Data.Strings == null ? null : string.Join(", ", t.Data.Strings)); });
-    o.Data.Strings = new ObservableCollection<string> { "A" };
+    o.Bind<Test, Test, ObservableCollection<string>>(
+      o,
+      [nameof(Test.Data), nameof(TestData.Strings)],
+      [s => (s as Test)?.Data, s => (s as TestData)?.Strings],
+      (t, c, e) => { list.Add(c == null ? null : string.Join(", ", c)); });
+    o.Data.Strings = ["A"];
     CollectionAssert.AreEqual(new[] { "A" }, list);
   }
 
   [TestMethod]
   public void C4_NestedCollection_DataReplaced() {
     var list = new List<string?>();
-    var o = new Test { Data = new TestData { Strings = new ObservableCollection<string> { "A" } } };
-    o.BindCollectionNested(o, x => x.Data.Strings, (t, e) => { list.Add(t.Data.Strings == null ? null : string.Join(", ", t.Data.Strings)); });
-    o.Data = new TestData { Strings = new ObservableCollection<string> { "B", "C" } };
+    var o = new Test { Data = new TestData { Strings = ["A"] } };
+    o.Bind<Test, Test, ObservableCollection<string>>(
+      o,
+      [nameof(Test.Data), nameof(TestData.Strings)],
+      [s => (s as Test)?.Data, s => (s as TestData)?.Strings],
+      (t, c, e) => { list.Add(c == null ? null : string.Join(", ", c)); });
+    o.Data = new TestData { Strings = ["B", "C"] };
     Assert.AreEqual(2, list.Count);
   }
 }

57c9e62e4f3cf5d16c19ec169ba866bafd475660
martin-holy <martin.holy@hotmail.com>
2026-01-19 11:42:52 +0100

    [N] MathU: GreatestCommonDivisor

---
+++ src/MH.Utils/MathU.cs
@@ -0,0 +1,13 @@
+﻿namespace MH.Utils;
+
+public static class MathU {
+  public static int GreatestCommonDivisor(int a, int b) {
+    while (a != 0 && b != 0) {
+      if (a > b)
+        a %= b;
+      else
+        b %= a;
+    }
+    return a == 0 ? b : a;
+  }
+}

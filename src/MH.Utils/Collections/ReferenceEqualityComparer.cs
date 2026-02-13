ae0fa399bc464505f4c97e856a8ca342a23238c0
martin-holy <martin.holy@hotmail.com>
2026-02-13 15:29:03 +0100

    [N] Generic ReferenceEqualityComparer

---
+++ src/MH.Utils/Collections/ReferenceEqualityComparer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace MH.Utils.Collections;
+
+public sealed class ReferenceEqualityComparer<T> : IEqualityComparer<T> where T : class {
+  public static readonly ReferenceEqualityComparer<T> Instance = new();
+
+  private ReferenceEqualityComparer() { }
+
+  public bool Equals(T? x, T? y) => ReferenceEquals(x, y);
+
+  public int GetHashCode(T obj) => RuntimeHelpers.GetHashCode(obj);
+}

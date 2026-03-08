f1c6b15810b649e7d96dca1416f369a8c89c7b5b
martin-holy <martin.holy@hotmail.com>
2026-03-08 19:55:02 +0100

    [N] BindingScope

---
+++ src/MH.Utils/Disposables/BindingScope.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MH.Utils.Disposables;
+
+public sealed class BindingScope : IDisposable {
+  private readonly List<IDisposable> _items = new();
+
+  public void Add(IDisposable disposable) {
+    _items.Add(disposable);
+  }
+
+  public void Dispose() {
+    for (int i = 0; i < _items.Count; i++)
+      _items[i].Dispose();
+
+    _items.Clear();
+  }
+}

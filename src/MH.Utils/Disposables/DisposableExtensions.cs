504a20f0d029a28f4a517af0062c4ae7771a8a56
martin-holy <martin.holy@hotmail.com>
2026-03-09 13:59:53 +0100

    [N] DisposableExtensions: DisposeWith BindingScope

---
+++ src/MH.Utils/Disposables/DisposableExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MH.Utils.Disposables;
+
+public static class DisposableExtensions {
+  public static T DisposeWith<T>(this T disposable, BindingScope scope) where T : IDisposable {
+    scope.Add(disposable);
+    return disposable;
+  }
+}

d637338827b684725d42b55f03a126b794608e5e
martin-holy <martin.holy@hotmail.com>
2026-06-10 19:55:12 +0200

    [N] IDataSource

---
+++ src/MH.Utils/DB/DataSources/DataSource.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MH.Utils.DB.DataSources;
+
+public class DataSource(string name) : IDataSource {
+  public string Name { get; } = name;
+
+  public virtual void Load() => throw new NotImplementedException();
+  public virtual bool Save() => throw new NotImplementedException();
+}

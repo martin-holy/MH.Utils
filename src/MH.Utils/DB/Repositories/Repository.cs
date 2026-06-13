7ce4ea7799e3dec5b3b312e85b1756478ac324aa
martin-holy <martin.holy@hotmail.com>
2026-06-13 14:00:47 +0200

    [C] Repositories namespace

--- src/MH.Utils/DB/Repositories/Repository.cs
+++ src/MH.Utils/DB/Repositories/Repository.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace MH.Utils.DB;
+namespace MH.Utils.DB.Repositories;
 
 public interface IRepository {
   bool IsModified { get; set; }

a735416437cffcb94b490b1e04c06b1b31c3be61
martin-holy <martin.holy@hotmail.com>
2026-06-14 20:51:46 +0200

    [N] DB OneToOneRelation

---
+++ src/MH.Utils/DB/Relations/OneToOneRelation.cs
@@ -0,0 +1,8 @@
+﻿using MH.Utils.DB.Repositories;
+
+namespace MH.Utils.DB.Relations;
+
+public class OneToOneRelation<TA, TB>(IRepository<TA> repoA, IRepository<TB> repoB) : Relation, IRelation<TA, TB> {
+  public IRepository<TA> RepositoryA { get; } = repoA;
+  public IRepository<TB> RepositoryB { get; } = repoB;
+}

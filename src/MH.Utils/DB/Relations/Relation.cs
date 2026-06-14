9e49337b3b482006f2a01f4eb0e3734b133aeaf0
martin-holy <martin.holy@hotmail.com>
2026-06-14 20:50:47 +0200

    [N] DB Relation

---
+++ src/MH.Utils/DB/Relations/Relation.cs
@@ -0,0 +1,31 @@
+﻿using MH.Utils.DB.Repositories;
+
+namespace MH.Utils.DB.Relations;
+
+public interface IRelation {
+  bool IsModified { get; set; }
+  bool AreRelationPropsModified { get; set; }
+  int ChangesCount { get; }
+}
+
+public interface IRelation<TA, TB> : IRelation {
+  public IRepository<TA> RepositoryA { get; }
+  public IRepository<TB> RepositoryB { get; }
+}
+
+public class Relation : IRelation {
+  private bool _isModified;
+
+  public bool IsModified { get => _isModified; set => _setIsModified(value); }
+  public bool AreRelationPropsModified { get; set; }
+  public int ChangesCount { get; private set; }
+
+  private void _setIsModified(bool value) {
+    _isModified = value;
+
+    if (value)
+      ChangesCount++;
+    else
+      ChangesCount = 0;
+  }
+}

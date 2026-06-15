24990979abb85a10dac6c2938ce8fb8fa12a371a
martin-holy <martin.holy@hotmail.com>
2026-06-15 21:51:16 +0200

    [N] DB DbTrackable

---
+++ src/MH.Utils/DB/DbTrackable.cs
@@ -0,0 +1,24 @@
+﻿namespace MH.Utils.DB;
+
+public interface IDbTrackable {
+  bool IsModified { get; set; }
+  bool ArePropsModified { get; set; }
+  int ChangesCount { get; }
+}
+
+public class DbTrackable : IDbTrackable {
+  private bool _isModified;
+
+  public bool IsModified { get => _isModified; set => _setIsModified(value); }
+  public bool ArePropsModified { get; set; }
+  public int ChangesCount { get; private set; }
+
+  private void _setIsModified(bool value) {
+    _isModified = value;
+
+    if (value)
+      ChangesCount++;
+    else
+      ChangesCount = 0;
+  }
+}

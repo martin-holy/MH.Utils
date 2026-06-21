ff8c83a93cf458e60015585f3f80b179346930a1
martin-holy <martin.holy@hotmail.com>
2026-06-21 14:27:08 +0200

    [N] IdParserTests

---
+++ tests/MH.Utils.Tests/DB/IdParserTests.cs
@@ -0,0 +1,33 @@
+﻿using MH.Utils.DB;
+
+namespace MH.Utils.Tests.DB;
+
+[TestClass]
+public class IdParserTests {
+  [TestMethod]
+  public void IdParser_Parse_Id() {
+    int id = CsvParser.ParseInt("487");
+    Assert.AreEqual(487, id);
+  }
+
+  [TestMethod]
+  public void IdParser_Parse_IdOrDefault1() {
+    int id = CsvParser.ParseIntOrDefault("487", 4);
+    Assert.AreEqual(487, id);
+  }
+
+  [TestMethod]
+  public void IdParser_Parse_IdOrDefault2() {
+    int id = CsvParser.ParseIntOrDefault(string.Empty, 4);
+    Assert.AreEqual(4, id);
+  }
+
+  [TestMethod]
+  public void IdParser_Parse_Ids() {
+    List<int> result = [];
+
+    CsvParser.ParseInts("487,499,46787,97974,789974", result, static (state, id) => state.Add(id));
+
+    CollectionAssert.AreEqual(new[] { 487, 499, 46787, 97974, 789974 }, result);
+  }
+}

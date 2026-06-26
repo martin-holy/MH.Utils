8775bd46a348fb797e02d859b59e340d3fd89dc5
martin-holy <martin.holy@hotmail.com>
2026-06-26 21:13:05 +0200

    [N] TreeItemExtensions: HasVisibleChildren

---
+++ src/MH.Utils/Extensions/TreeItemExtensions.cs
@@ -0,0 +1,13 @@
+﻿using MH.Utils.Interfaces;
+
+namespace MH.Utils.Extensions;
+
+public static class TreeItemExtensions {
+  public static bool HasVisibleChildren(this ITreeItem self) {
+    foreach (var child in self.Items)
+      if (!child.IsHidden)
+        return true;
+
+    return false;
+  }
+}

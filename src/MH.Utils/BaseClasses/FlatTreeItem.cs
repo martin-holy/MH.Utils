01dca655e5220b27d9454c3e328c5b59c9cc3280
martin-holy <martin.holy@hotmail.com>
2026-06-26 21:14:29 +0200

    [N] FlatTreeItem: HasVisibleChildren prop

--- src/MH.Utils/BaseClasses/FlatTreeItem.cs
+++ src/MH.Utils/BaseClasses/FlatTreeItem.cs
@@ -3,9 +3,12 @@ using System;
 
 namespace MH.Utils.BaseClasses;
 
-public class FlatTreeItem(ITreeItem treeItem, int level) : IEquatable<FlatTreeItem> {
+public class FlatTreeItem(ITreeItem treeItem, int level) : ObservableObject, IEquatable<FlatTreeItem> {
+  private bool _hasVisibleChildren;
+
   public ITreeItem TreeItem { get; } = treeItem;
   public int Level { get; } = level;
+  public bool HasVisibleChildren { get => _hasVisibleChildren; internal set { _hasVisibleChildren = value; OnPropertyChanged(); } }
 
   public bool Equals(FlatTreeItem? other) => other is not null && ReferenceEquals(TreeItem, other.TreeItem);
   public override bool Equals(object? obj) => obj?.GetType() == GetType() && ReferenceEquals(TreeItem, ((FlatTreeItem)obj).TreeItem);

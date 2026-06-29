645eb0b0230dfe66450fc1535b09ed1608ec07f2
martin-holy <martin.holy@hotmail.com>
2026-06-29 19:44:51 +0200

    [N] TreeItemExtensions: ExpandToRoot

--- src/MH.Utils/Extensions/TreeItemExtensions.cs
+++ src/MH.Utils/Extensions/TreeItemExtensions.cs
@@ -3,6 +3,15 @@
 namespace MH.Utils.Extensions;
 
 public static class TreeItemExtensions {
+  public static void ExpandToRoot(this ITreeItem self) {
+    var parent = self.Parent;
+
+    while (parent != null) {
+      parent.IsExpanded = true;
+      parent = parent.Parent;
+    }
+  }
+
   public static bool HasVisibleChildren(this ITreeItem self) {
     foreach (var child in self.Items)
       if (!child.IsHidden)

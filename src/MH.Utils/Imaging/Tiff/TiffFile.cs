b7a47a8e70d98cd191ca676fb408627f35c2b582
martin-holy <martin.holy@hotmail.com>
2026-07-12 20:30:22 +0200

    [N] TiffFile

---
+++ src/MH.Utils/Imaging/Tiff/TiffFile.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace MH.Utils.Imaging.Tiff;
+
+public sealed class TiffFile(TiffIfd ifd0) {
+  public TiffIfd Ifd0 { get; } = ifd0;
+  public List<Hole> Holes { get; } = [];
+}

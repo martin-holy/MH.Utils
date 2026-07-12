513aff0f2b887b51ace535debdf10f7ff3d5dc7d
martin-holy <martin.holy@hotmail.com>
2026-07-12 20:28:08 +0200

    [N] TiffEntry

---
+++ src/MH.Utils/Imaging/Tiff/TiffEntry.cs
@@ -0,0 +1,22 @@
+﻿namespace MH.Utils.Imaging.Tiff;
+
+public sealed class TiffEntry(ushort tag, ushort type, uint count) {
+  public ushort Tag { get; } = tag;
+  public ushort Type { get; } = type;
+  public uint Count { get; set; } = count;
+  public TiffObject? Value { get; set; }
+  public TiffIfd? SubIfd { get; set; }
+
+  public void Write(TiffWriter writer) {
+    writer.WriteUInt16(Tag);
+    writer.WriteUInt16(Type);
+    writer.WriteUInt32(Count);
+
+    if (SubIfd != null)
+      writer.WriteReference(SubIfd);
+    else if (Value is InlineValue inline)
+      writer.WriteInlineValue(inline.Data);
+    else
+      writer.WriteReference(Value!);
+  }
+}

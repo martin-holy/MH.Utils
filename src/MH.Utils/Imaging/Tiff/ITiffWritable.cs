130a8b0ba64f76ac9fe66a39f0b3f298c4430a4d
martin-holy <martin.holy@hotmail.com>
2026-07-12 21:01:48 +0200

    [N] ITiffWritable

---
+++ src/MH.Utils/Imaging/Tiff/ITiffWritable.cs
@@ -0,0 +1,10 @@
+﻿namespace MH.Utils.Imaging.Tiff;
+
+public interface ITiffWritable {
+  uint OriginalOffset { get; }
+  uint WriteOffset { get; set; }
+  int OriginalSize { get; }
+  int CurrentSize { get; }
+  int ShrinkableBytes { get; }
+  void Write(TiffWriter writer);
+}

308b5a28b5435d6cc0955592168a70bdd7c1f3e6
martin-holy <martin.holy@hotmail.com>
2026-07-12 20:31:34 +0200

    [N] TiffLayoutBuilder

---
+++ src/MH.Utils/Imaging/Tiff/TiffLayoutBuilder.cs
@@ -0,0 +1,32 @@
+﻿namespace MH.Utils.Imaging.Tiff;
+
+public static class TiffLayoutBuilder {
+  public static TiffLayout Build(TiffFile file) {
+    var layout = new TiffLayout();
+
+    _collectIfd(layout, file.Ifd0);
+
+    layout.Items.Sort(static (a, b) => a.OriginalOffset.CompareTo(b.OriginalOffset));
+
+    return layout;
+  }
+
+  private static void _collectIfd(TiffLayout layout, TiffIfd ifd) {
+    layout.Items.Add(ifd);
+
+    foreach (var entry in ifd.Entries) {
+      if (entry.SubIfd != null) {
+        _collectIfd(layout, entry.SubIfd);
+        continue;
+      }
+
+      if (entry.Value is InlineValue)
+        continue;
+
+      layout.Items.Add(entry.Value!);
+    }
+
+    if (ifd.NextIfd != null)
+      _collectIfd(layout, ifd.NextIfd);
+  }
+}

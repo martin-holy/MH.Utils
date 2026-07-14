b81d87e86b9fdd29a2a029cd9661ea0e2fa923f9
martin-holy <martin.holy@hotmail.com>
2026-07-14 08:20:27 +0200

    [U] TiffLayout

--- src/MH.Utils/Imaging/Tiff/TiffLayout.cs
+++ src/MH.Utils/Imaging/Tiff/TiffLayout.cs
@@ -4,5 +4,15 @@ namespace MH.Utils.Imaging.Tiff;
 
 public sealed class TiffLayout {
   public List<ITiffWritable> Items { get; } = [];
-  public List<Hole> Holes { get; } = [];
+  public List<TiffLayoutHole> Holes { get; } = [];
+
+  public TiffLayoutHole? FindHoleAfter(ITiffWritable item) {
+    uint end = item.OriginalOffset + (uint)item.OriginalSize;
+
+    foreach (var hole in Holes)
+      if (hole.OriginalOffset == end)
+        return hole;
+
+    return null;
+  }
 }

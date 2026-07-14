20087949c04c1299de9de4360143fdca740c3538
martin-holy <martin.holy@hotmail.com>
2026-07-14 08:22:14 +0200

    [N] TiffLayoutPlanner

---
+++ src/MH.Utils/Imaging/Tiff/TiffLayoutPlanner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MH.Utils.Imaging.Tiff;
+
+public static class TiffLayoutPlanner {
+  public static void Plan(TiffLayout layout) {
+    var pending = 0;
+    var holeIndex = 0;
+
+    foreach (var item in layout.Items) {
+      var delta = item.CurrentSize - item.OriginalSize;
+
+      if (delta > 0)
+        pending += delta;
+
+      while (holeIndex < layout.Holes.Count) {
+        var hole = layout.Holes[holeIndex];
+
+        if (hole.OriginalOffset != item.OriginalOffset + item.OriginalSize)
+          break;
+
+        var consume = Math.Min(hole.Size, pending);
+        hole.Consume(consume);
+        pending -= consume;
+        holeIndex++;
+      }
+
+      if (pending == 0)
+        continue;
+
+      if (item is PaddingValue padding) {
+        var consume = Math.Min(padding.Data.Length, pending);
+        padding.Consume(consume);
+        pending -= consume;
+      }
+    }
+  }
+}

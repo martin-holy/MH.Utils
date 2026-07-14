6bddd02e0b5221c410d5f5db4768aa8f4037aa7b
martin-holy <martin.holy@hotmail.com>
2026-07-14 08:19:07 +0200

    [N] TiffLayoutHole

---
+++ src/MH.Utils/Imaging/Tiff/TiffLayoutHole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MH.Utils.Imaging.Tiff;
+
+public sealed class TiffLayoutHole(uint offset, byte[] data) {
+  public uint OriginalOffset { get; } = offset;
+  public byte[] Data { get; private set; } = data;
+
+  public int Size => Data.Length;
+
+  public void Consume(int bytes) {
+    byte[] data = Data;
+    Array.Resize(ref data, data.Length - bytes);
+    Data = data;
+  }
+}

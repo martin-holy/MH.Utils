b1c0548a687133ca849779be89fa3882627c8430
martin-holy <martin.holy@hotmail.com>
2026-07-14 08:22:36 +0200

    [U] TiffResolver

--- src/MH.Utils/Imaging/Tiff/TiffResolver.cs
+++ src/MH.Utils/Imaging/Tiff/TiffResolver.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Buffers.Binary;
-using System.Collections.Generic;
 
 namespace MH.Utils.Imaging.Tiff;
 
 public static class TiffResolver {
   public static void Resolve(TiffReader reader, TiffFile file) {
     _resolveIfd(reader, file.Ifd0, false);
-    _resolveHoles(reader, file, file.Ifd0);
   }
 
   private static void _resolveIfd(TiffReader reader, TiffIfd ifd, bool isIfd1) {
@@ -49,57 +47,6 @@ public static class TiffResolver {
     uint length = _readUInt32(reader.IsLittleEndian, lengthValue.Data);
 
     offsetEntry.Value = new JpegValue(offset, reader.GetSpan(offset, checked((int)length)).ToArray());
-  }
-
-  private static void _resolveHoles(TiffReader reader, TiffFile file, TiffIfd ifd) {
-    _insertHoles(reader, file, ifd);
-
-    foreach (var entry in ifd.Entries) {
-      if (entry.SubIfd != null)
-        _resolveHoles(reader, file, entry.SubIfd);
-    }
-
-    if (ifd.NextIfd != null)
-      _resolveHoles(reader, file, ifd.NextIfd);
-  }
-
-  private static void _insertHoles(TiffReader reader, TiffFile file, TiffIfd ifd) {
-    var values = new List<TiffObject>();
-
-    foreach (var entry in ifd.Entries) {
-      if (entry.Value is InlineValue)
-        continue;
-
-      if (entry.Value != null)
-        values.Add(entry.Value);
-    }
-
-    values.Sort(static (a, b) => a.OriginalOffset.CompareTo(b.OriginalOffset));
-
-    for (int i = 0; i < values.Count - 1; i++) {
-      var current = values[i];
-      var next = values[i + 1];
-
-      uint end = current.OriginalOffset + (uint)current.OriginalSize;
-
-      if (next.OriginalOffset <= end)
-        continue;
-
-      int size = checked((int)(next.OriginalOffset - end));
-
-      values.Insert(i + 1,
-        new Hole(
-          end,
-          reader.GetSpan(end, size).ToArray()));
-
-      i++;
-    }
-
-    foreach (var value in values) {
-      if (value is Hole hole) {
-        file.Holes.Add(hole);
-      }
-    }
   }
 
   private static uint _readUInt32(bool littleEndian, byte[] bytes) {

90a1fd83dfd71ef8469bf052ad689f92230ded31
martin-holy <martin.holy@hotmail.com>
2026-07-16 07:59:14 +0200

    [N] TiffEntryData

---
+++ src/MH.Utils/Imaging/Tiff/TiffEntryData.cs
@@ -0,0 +1,13 @@
+﻿namespace MH.Utils.Imaging.Tiff;
+
+public readonly record struct TiffEntryData(uint EntryOffset, ushort Tag, ushort Type, uint Count, uint ValueOrOffset);
+
+public static class TiffEntryDataExtensions {
+  public static TiffEntryData? FindEntry(this TiffEntryData[] entries, ExifTag tag) {
+    foreach (var entry in entries)
+      if (entry.Tag == (ushort)tag)
+        return entry;
+
+    return null;
+  }
+}

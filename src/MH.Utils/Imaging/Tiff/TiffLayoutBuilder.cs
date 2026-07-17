87eae3f261c7b3a3ed39134ce59565c51bd4ef54
martin-holy <martin.holy@hotmail.com>
2026-07-17 21:10:42 +0200

    [U] TiffLayoutBuilder

--- src/MH.Utils/Imaging/Tiff/TiffLayoutBuilder.cs
+++ src/MH.Utils/Imaging/Tiff/TiffLayoutBuilder.cs
@@ -1,11 +1,24 @@
 ﻿namespace MH.Utils.Imaging.Tiff;
 
 public static class TiffLayoutBuilder {
-  public static TiffLayout Build(TiffFile file, TiffReader reader) {
+  public static TiffLayout Build(TiffFile file, TiffReader? reader) {
     var layout = new TiffLayout();
+
     _collectIfd(layout, file.Ifd0);
-    layout.Items.Sort(static (a, b) => a.OriginalOffset.CompareTo(b.OriginalOffset));
-    _findHoles(layout, reader);
+
+    layout.Items.Sort(static (a, b) => {
+      if (a.OriginalOffset is uint oa) {
+        if (b.OriginalOffset is uint ob)
+          return oa.CompareTo(ob);
+
+        return -1;
+      }
+
+      return b.OriginalOffset is null ? 0 : 1;
+    });
+
+    if (reader != null)
+      _findHoles(layout, reader);
 
     return layout;
   }
@@ -32,8 +45,11 @@ public static class TiffLayoutBuilder {
   private static void _findHoles(TiffLayout layout, TiffReader reader) {
     for (int i = 0; i < layout.Items.Count - 1; i++) {
       var current = layout.Items[i];
+
+      if (current.OriginalOffset == null) continue;
+
       var next = layout.Items[i + 1];
-      uint end = current.OriginalOffset + (uint)current.OriginalSize;
+      uint end = (uint)current.OriginalOffset + (uint)current.OriginalSize;
 
       if (next.OriginalOffset > end) {
         var length = checked((int)(next.OriginalOffset - end));

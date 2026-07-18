964cd84293f7e6e529fbace40d277975ec697aa6
martin-holy <martin.holy@hotmail.com>
2026-07-18 17:55:45 +0200

    [U] TiffObjects

--- src/MH.Utils/Imaging/Tiff/TiffObjects.cs
+++ src/MH.Utils/Imaging/Tiff/TiffObjects.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace MH.Utils.Imaging.Tiff;
 
@@ -11,44 +10,6 @@ public abstract class TiffObject(uint? originalOffset) : ITiffWritable {
   public virtual int CurrentSize => OriginalSize;
 
   public abstract void Write(TiffWriter writer);
-}
-
-public sealed class TiffIfd(uint? originalOffset, List<TiffEntry> entries) : TiffObject(originalOffset) {
-  public List<TiffEntry> Entries { get; } = entries;
-  public TiffIfd? NextIfd { get; set; }
-
-  // TODO maybe BUG if Entries change
-  public override int OriginalSize =>
-    2 + Entries.Count * 12 + 4;
-
-  public override void Write(TiffWriter writer) {
-    WriteOffset = (uint)writer.Position;
-
-    writer.WriteUInt16((ushort)Entries.Count);
-
-    foreach (var entry in Entries)
-      entry.Write(writer);
-
-    if (NextIfd == null)
-      writer.WriteUInt32(0);
-    else
-      writer.WriteReference(NextIfd);
-  }
-
-  public TiffEntry? FindEntry(ExifTag tag) {
-    foreach (var entry in Entries) {
-      if (entry.Tag == (ushort)tag)
-        return entry;
-
-      if (entry.SubIfd != null) {
-        var found = entry.SubIfd.FindEntry(tag);
-        if (found != null)
-          return found;
-      }
-    }
-
-    return NextIfd?.FindEntry(tag);
-  }
 }
 
 public class DataValue(uint? originalOffset, byte[] data) : TiffObject(originalOffset) {

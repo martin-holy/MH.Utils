9debc475552c651d56e0d8fddd188824337df2ce
martin-holy <martin.holy@hotmail.com>
2026-07-18 18:30:02 +0200

    [U] TiffLayoutBuilder

--- src/MH.Utils/Imaging/Tiff/TiffLayoutBuilder.cs
+++ src/MH.Utils/Imaging/Tiff/TiffLayoutBuilder.cs
@@ -1,21 +1,12 @@
-﻿namespace MH.Utils.Imaging.Tiff;
+﻿using System.Collections.Generic;
+
+namespace MH.Utils.Imaging.Tiff;
 
 public static class TiffLayoutBuilder {
   public static TiffLayout Build(TiffFile file, TiffReader? reader) {
     var layout = new TiffLayout();
 
     _collectIfd(layout, file.Ifd0);
-
-    layout.Items.Sort(static (a, b) => {
-      if (a.OriginalOffset is uint oa) {
-        if (b.OriginalOffset is uint ob)
-          return oa.CompareTo(ob);
-
-        return -1;
-      }
-
-      return b.OriginalOffset is null ? 0 : 1;
-    });
 
     if (reader != null)
       _findHoles(layout, reader);
@@ -43,18 +34,26 @@ public static class TiffLayoutBuilder {
   }
 
   private static void _findHoles(TiffLayout layout, TiffReader reader) {
-    for (int i = 0; i < layout.Items.Count - 1; i++) {
-      var current = layout.Items[i];
+    var items = new List<TiffObject>(layout.Items.Count);
 
-      if (current.OriginalOffset == null) continue;
+    foreach (var item in layout.Items)
+      if (item.OriginalOffset != null)
+        items.Add((TiffObject)item);
 
-      var next = layout.Items[i + 1];
-      uint end = (uint)current.OriginalOffset + (uint)current.OriginalSize;
+    items.Sort(static (a, b) =>
+      a.OriginalOffset!.Value.CompareTo(b.OriginalOffset!.Value));
 
-      if (next.OriginalOffset > end) {
-        var length = checked((int)(next.OriginalOffset - end));
-        layout.Holes.Add(new TiffLayoutHole(end, reader.GetSpan(end, length).ToArray()));
-      }
+    for (int i = 0; i < items.Count - 1; i++) {
+      var current = items[i];
+      var next = items[i + 1];
+      uint end = current.OriginalOffset!.Value + (uint)current.OriginalSize;
+
+      if (next.OriginalOffset!.Value <= end)
+        continue;
+
+      int length = checked((int)(next.OriginalOffset!.Value - end));
+
+      layout.Holes.Add(new TiffLayoutHole(end, reader.GetSpan(end, length).ToArray()));
     }
   }
 }

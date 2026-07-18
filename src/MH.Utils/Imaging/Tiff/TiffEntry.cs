67619da3c01981fa1d2ded712e2c66a8e131e9c8
martin-holy <martin.holy@hotmail.com>
2026-07-18 17:23:45 +0200

    [U] TiffEntry

--- src/MH.Utils/Imaging/Tiff/TiffEntry.cs
+++ src/MH.Utils/Imaging/Tiff/TiffEntry.cs
@@ -7,6 +7,8 @@ public sealed class TiffEntry(ushort tag, ushort type, uint count) {
   public TiffObject? Value { get; set; }
   public TiffIfd? SubIfd { get; set; }
 
+  public TiffEntry(ExifTag tag, TiffType type, int count) : this((ushort)tag, (ushort)type, (uint)count) { }
+
   public void Write(TiffWriter writer) {
     writer.WriteUInt16(Tag);
     writer.WriteUInt16(Type);

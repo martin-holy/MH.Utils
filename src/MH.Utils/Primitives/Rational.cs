0acf5e0cec2d5589eb3391d045e290e3849128fa
martin-holy <martin.holy@hotmail.com>
2026-07-25 14:18:04 +0200

    [N] RationalExtensions: ToDouble

--- src/MH.Utils/Primitives/Rational.cs
+++ src/MH.Utils/Primitives/Rational.cs
@@ -1,3 +1,11 @@
 ﻿namespace MH.Utils.Primitives;
 
 public readonly record struct Rational(uint Numerator, uint Denominator);
+
+public static class RationalExtensions {
+  public static double ToDouble(this Rational value) {
+    if (value.Denominator == 0) return 0;
+
+    return (double)value.Numerator / value.Denominator;
+  }
+}

0db527d02acfc8f8508db8344c1c8bcb28f0b1b4
martin-holy <martin.holy@hotmail.com>
2026-07-25 07:16:16 +0200

    [U] TiffFile

--- src/MH.Utils/Imaging/Tiff/TiffFile.cs
+++ src/MH.Utils/Imaging/Tiff/TiffFile.cs
@@ -1,10 +1,21 @@
 ﻿namespace MH.Utils.Imaging.Tiff;
 
-public sealed class TiffFile(TiffIfd ifd0) {
+public sealed class TiffFile(TiffIfd ifd0, bool isLittleEndian) {
+  public bool IsLittleEndian { get; } = isLittleEndian;
   public TiffIfd Ifd0 { get; } = ifd0;
   public TiffIfd? ExifIfd { get; set; }
   public TiffIfd? GpsIfd { get; set; }
 
   public static TiffFile CreateEmpty() =>
-    new(new TiffIfd(null, []));
+    new(new TiffIfd(null, []), true);
+
+  public TiffIfd GetOrCreateExifIfd() {
+    ExifIfd ??= Ifd0.CreateSubIfd(ExifTag.ExifIfd);
+    return ExifIfd;
+  }
+
+  public TiffIfd GetOrCreateGpsIfd() {
+    GpsIfd ??= Ifd0.CreateSubIfd(ExifTag.GpsIfd);
+    return GpsIfd;
+  }
 }

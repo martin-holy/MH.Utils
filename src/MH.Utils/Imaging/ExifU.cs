84e7a20c2b75aecebdfcdba02a40b587c94579ee
martin-holy <martin.holy@hotmail.com>
2026-07-25 07:21:36 +0200

    [U] ExifU

--- src/MH.Utils/Imaging/ExifU.cs
+++ src/MH.Utils/Imaging/ExifU.cs
@@ -1,6 +1,4 @@
-﻿using MH.Utils.Imaging.Tiff;
-using System;
-using System.IO;
+﻿using System;
 
 namespace MH.Utils.Imaging;
 
@@ -9,39 +7,4 @@ public static class ExifU {
   public static ReadOnlySpan<byte> AsciiHeader => "ASCII\0\0\0"u8;
   public static ReadOnlySpan<byte> UnicodeHeader => "UNICODE\0"u8;
   public static ReadOnlySpan<byte> JisHeader => "JIS\0\0\0\0\0"u8;
-
-  public static bool WriteExifToJpeg(string srcPath, ImageMetadata metadata) {
-    var tmpPath = srcPath + ".tmp";
-
-    try {
-      using (var input = File.OpenRead(srcPath))
-      using (var output = File.Create(tmpPath)) {
-        TiffWriter.WriteExifToJpeg(input, output, MetadataToTiff(metadata));
-      }
-
-      File.Delete(srcPath);
-      File.Move(tmpPath, srcPath);
-
-      return true;
-    }
-    catch (Exception ex) {
-      Log.Error(ex, srcPath);
-
-      try {
-        if (File.Exists(tmpPath))
-          File.Delete(tmpPath);
-      }
-      catch { }
-
-      return false;
-    }
-  }
-
-  public static byte[] MetadataToTiff(ImageMetadata metadata) {
-    var layout = TiffLayoutBuilder.Build(metadata.TiffFile, metadata.Reader);
-    TiffLayoutPlanner.Plan(layout);
-    var tiff = TiffSerializer.Serialize(metadata.TiffFile, metadata.Reader?.IsLittleEndian ?? true);
-
-    return tiff;
-  }
 }

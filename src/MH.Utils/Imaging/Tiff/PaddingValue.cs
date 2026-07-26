2a2e80e36a3ef2124c886a0e947af82a9deed24b
martin-holy <martin.holy@hotmail.com>
2026-07-26 06:34:05 +0200

    [U] PaddingValue

--- src/MH.Utils/Imaging/Tiff/PaddingValue.cs
+++ src/MH.Utils/Imaging/Tiff/PaddingValue.cs
@@ -5,21 +5,19 @@ namespace MH.Utils.Imaging.Tiff;
 public sealed class PaddingValue(uint? originalOffset, byte[] data) : DataValue(originalOffset, data) {
   public int Consume(int requested) {
     int consumed = Math.Min(requested, Data.Length);
-
     if (consumed == 0) return 0;
-
-    byte[] data = Data;
-    Array.Resize(ref data, data.Length - consumed);
-    Data = data;
-
+    _resizeData(Data.Length - consumed);
     return consumed;
   }
 
   public void Extend(int bytes) {
     if (bytes <= 0) return;
+    _resizeData(Data.Length + bytes);
+  }
 
+  private void _resizeData(int length) {
     byte[] data = Data;
-    Array.Resize(ref data, data.Length + bytes);
+    Array.Resize(ref data, length);
     Data = data;
   }
 }

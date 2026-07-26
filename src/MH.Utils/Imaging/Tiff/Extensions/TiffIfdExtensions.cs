f3a5f1339225c95eaf8ef3fdcb290956c121b4d7
martin-holy <martin.holy@hotmail.com>
2026-07-26 08:39:38 +0200

    [N] TiffIfdExtensions

---
+++ src/MH.Utils/Imaging/Tiff/Extensions/TiffIfdExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MH.Utils.IO;
+using MH.Utils.Primitives;
+using System.Text;
+
+namespace MH.Utils.Imaging.Tiff.Extensions;
+
+public static class TiffIfdExtensions {
+  public static TiffIfd SetRationals(this TiffIfd ifd, ExifTag tag, Rational[] values, bool littleEndian) {
+    var count = values.Length;
+    var data = BinarySpanWriter.GetBytes(values, littleEndian);
+    ifd.SetDataEntry(tag, TiffType.Rational, data, count);
+    return ifd;
+  }
+
+  public static TiffIfd SetAscii(this TiffIfd ifd, ExifTag tag, string value) {
+    ifd.SetDataEntry(tag, TiffType.Ascii, Encoding.ASCII.GetBytes(value + '\0'));
+    return ifd;
+  }
+}

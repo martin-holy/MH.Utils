e400fae4540b343126d25d62087a4f13cab69094
martin-holy <martin.holy@hotmail.com>
2026-07-27 17:04:59 +0200

    [N] XmpNs

---
+++ src/MH.Utils/Imaging/Xmp/XmpNs.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Linq;
+
+namespace MH.Utils.Imaging.Xmp;
+
+public static class XmpNs {
+  public static readonly XNamespace Xmp = "http://ns.adobe.com/xap/1.0/";
+  public static readonly XNamespace Dc = "http://purl.org/dc/elements/1.1/";
+  public static readonly XNamespace Exif = "http://ns.adobe.com/exif/1.0/";
+  public static readonly XNamespace Tiff = "http://ns.adobe.com/tiff/1.0/";
+  public static readonly XNamespace Mp = "http://ns.microsoft.com/photo/1.2/";
+  public static readonly XNamespace MpRi = "http://ns.microsoft.com/photo/1.2/t/RegionInfo#";
+  public static readonly XNamespace MpReg = "http://ns.microsoft.com/photo/1.2/t/Region#";
+}

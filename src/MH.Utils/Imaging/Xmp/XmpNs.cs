3bf9289e1d939c0e2d7d0efd9ef5138459e2d38f
martin-holy <martin.holy@hotmail.com>
2026-07-28 21:00:14 +0200

    [U] XmpNs

--- src/MH.Utils/Imaging/Xmp/XmpNs.cs
+++ src/MH.Utils/Imaging/Xmp/XmpNs.cs
@@ -7,6 +7,7 @@ public static class XmpNs {
   public static readonly XNamespace Dc = "http://purl.org/dc/elements/1.1/";
   public static readonly XNamespace Exif = "http://ns.adobe.com/exif/1.0/";
   public static readonly XNamespace Tiff = "http://ns.adobe.com/tiff/1.0/";
+  public static readonly XNamespace Rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
   public static readonly XNamespace Mp = "http://ns.microsoft.com/photo/1.2/";
   public static readonly XNamespace MpRi = "http://ns.microsoft.com/photo/1.2/t/RegionInfo#";
   public static readonly XNamespace MpReg = "http://ns.microsoft.com/photo/1.2/t/Region#";

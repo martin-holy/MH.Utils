85268724553240f09a840887254cec39fe6ac0de
martin-holy <martin.holy@hotmail.com>
2026-08-03 21:21:55 +0200

    [N] MpRegion

---
+++ src/MH.Utils/Imaging/Xmp/MpRegion.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+
+namespace MH.Utils.Imaging.Xmp;
+
+public sealed class MpRegion(XElement element) {
+  public XElement Element { get; } = element;
+
+  public string? PersonDisplayName {
+    get => (string?)Element.Attribute(XmpNs.MpReg + "PersonDisplayName");
+    set => Element.SetAttributeValue(XmpNs.MpReg + "PersonDisplayName", value);
+  }
+
+  public string? Rectangle {
+    get => (string?)Element.Attribute(XmpNs.MpReg + "Rectangle");
+    set => Element.SetAttributeValue(XmpNs.MpReg + "Rectangle", value);
+  }
+}

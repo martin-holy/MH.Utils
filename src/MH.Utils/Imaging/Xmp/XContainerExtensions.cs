5344687e96febe3cacd8498a3412624c4ed171da
martin-holy <martin.holy@hotmail.com>
2026-08-09 12:49:01 +0200

    [N] XContainerExtensions: GetXmpArray

---
+++ src/MH.Utils/Imaging/Xmp/XContainerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace MH.Utils.Imaging.Xmp;
+
+public static class XContainerExtensions {
+  public static IEnumerable<XElement> GetXmpArray(this XContainer container, XName arrayName) =>
+    container
+      .Descendants(arrayName)
+      .Elements()
+      .Where(e =>
+        e.Name == XmpNs.Rdf + "Bag" ||
+        e.Name == XmpNs.Rdf + "Seq" ||
+        e.Name == XmpNs.Rdf + "Alt")
+      .Elements(XmpNs.Rdf + "li");
+}

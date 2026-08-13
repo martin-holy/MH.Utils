0722c1204e8010f94dafc7bef887817bf325ce96
martin-holy <martin.holy@hotmail.com>
2026-08-13 21:03:12 +0200

    [N] XmpExtensionsTests

---
+++ tests/MH.Utils.Tests/Imaging/Xmp/XmpExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using MH.Utils.Imaging.Xmp;
+using System.Xml.Linq;
+
+namespace MH.Utils.Tests.Imaging.Xmp;
+
+[TestClass]
+public class XmpExtensionsTests {
+  [TestMethod]
+  public void GetXmpProperty_ReadsAttribute() {
+    var element = new XElement(XmpNs.Rdf + "li",
+      new XAttribute(XmpNs.MpReg + "PersonDisplayName","Martin"));
+
+    Assert.AreEqual("Martin", element.GetXmpProperty(XmpNs.MpReg + "PersonDisplayName"));
+  }
+
+  [TestMethod]
+  public void GetXmpProperty_ReadsDescriptionAttribute() {
+    var element = new XElement(XmpNs.Rdf + "li",
+      new XElement(XmpNs.Rdf + "Description",
+        new XAttribute(XmpNs.MpReg + "PersonDisplayName", "Martin")));
+
+    Assert.AreEqual("Martin", element.GetXmpProperty(XmpNs.MpReg + "PersonDisplayName"));
+  }
+
+  [TestMethod]
+  public void GetXmpProperty_ReadsElement() {
+    var element = new XElement(XmpNs.Rdf + "li",
+      new XElement(XmpNs.MpReg + "PersonDisplayName", "Martin"));
+
+    Assert.AreEqual("Martin", element.GetXmpProperty(XmpNs.MpReg + "PersonDisplayName"));
+  }
+
+  [TestMethod]
+  public void GetXmpProperty_ReadsDescriptionElement() {
+    var element = new XElement(XmpNs.Rdf + "li",
+      new XElement(XmpNs.Rdf + "Description",
+        new XElement(XmpNs.MpReg + "PersonDisplayName", "Martin")));
+
+    Assert.AreEqual("Martin", element.GetXmpProperty(XmpNs.MpReg + "PersonDisplayName"));
+  }
+}

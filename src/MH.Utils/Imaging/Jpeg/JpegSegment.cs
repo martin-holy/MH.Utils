9498c0b3bc8e69309968f2b1757e935cd22a3e7b
martin-holy <martin.holy@hotmail.com>
2026-08-15 20:46:27 +0200

    [N] JpegSegment

---
+++ src/MH.Utils/Imaging/Jpeg/JpegSegment.cs
@@ -0,0 +1,18 @@
+﻿namespace MH.Utils.Imaging.Jpeg;
+
+public sealed class JpegSegment {
+  public byte Marker { get; }
+  public long Position { get; }
+  public int Length { get; }
+  public long PayloadPosition { get; }
+  public int PayloadLength { get; }
+  internal byte[]? Payload { get; set; }
+
+  internal JpegSegment(byte marker, long position, int length, long payloadPosition, int payloadLength) {
+    Marker = marker;
+    Position = position;
+    Length = length;
+    PayloadPosition = payloadPosition;
+    PayloadLength = payloadLength;
+  }
+}

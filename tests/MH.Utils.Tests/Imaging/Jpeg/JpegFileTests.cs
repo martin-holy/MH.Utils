f4c8e534280839d12eadc24b1819ca1737c321a9
martin-holy <martin.holy@hotmail.com>
2026-08-16 11:40:32 +0200

    [U] JpegFileTests

--- tests/MH.Utils.Tests/Imaging/Jpeg/JpegFileTests.cs
+++ tests/MH.Utils.Tests/Imaging/Jpeg/JpegFileTests.cs
@@ -64,6 +64,7 @@ public class JpegFileTests {
 
     using var jpeg = new JpegFile(stream);
     var xmp = jpeg.Xmp;
+
     Assert.AreSame(xmp, jpeg.Xmp);
   }
 
@@ -248,8 +249,150 @@ public class JpegFileTests {
     Assert.AreEqual((ushort)6, jpeg.Exif.GetOrientation());
   }
 
-  private static ReadOnlySpan<byte> _exifHeader => "Exif\0\0"u8;
-  private static ReadOnlySpan<byte> _xmpHeader => "http://ns.adobe.com/xap/1.0/\0"u8;
+  [TestMethod]
+  public void JpegFile_ReadsExtendedXmp() {
+    const string guid = "0123456789ABCDEF0123456789ABCDEF";
+    const string comment = "Extended XMP description";
+
+    var xml = Encoding.UTF8.GetBytes(_createExtendedXmpXml(comment));
+    var mainXmp = _createApp1Xmp(_createMainXmpWithExtendedGuid(guid));
+    var extendedXmp = _createApp1ExtendedXmp(guid, xml.Length, 0, xml);
+
+    using var stream = new MemoryStream(
+      _createJpeg(mainXmp, _createApp(0xE2, 100), extendedXmp, _createSof(640, 480)));
+
+    using var jpeg = new JpegFile(stream);
+
+    Assert.AreEqual(comment, jpeg.Xmp.GetComment());
+  }
+
+  [TestMethod]
+  public void JpegFile_ReconstructsExtendedXmpFromMultipleChunks() {
+    const string guid = "0123456789ABCDEF0123456789ABCDEF";
+    const string comment = "This is a longer extended XMP description.";
+
+    var xml = Encoding.UTF8.GetBytes(_createExtendedXmpXml(comment));
+    var split = xml.Length / 2;
+    var first = xml[..split];
+    var second = xml[split..];
+
+    using var stream = new MemoryStream(
+      _createJpeg(
+        _createApp1Xmp(_createMainXmpWithExtendedGuid(guid)),
+        _createApp1ExtendedXmp(guid, xml.Length, 0, first),
+        _createApp1ExtendedXmp(guid, xml.Length, split, second),
+        _createSof(640, 480)));
+
+    using var jpeg = new JpegFile(stream);
+
+    Assert.AreEqual(comment, jpeg.Xmp.GetComment());
+  }
+
+  [TestMethod]
+  public void JpegFile_ReconstructsExtendedXmp_WhenChunksAreOutOfOrder() {
+    const string guid = "0123456789ABCDEF0123456789ABCDEF";
+    const string comment = "Out of order extended XMP";
+
+    var xml = Encoding.UTF8.GetBytes(_createExtendedXmpXml(comment));
+    var split = xml.Length / 2;
+    var first = xml[..split];
+    var second = xml[split..];
+
+    using var stream = new MemoryStream(
+      _createJpeg(
+        _createApp1Xmp(_createMainXmpWithExtendedGuid(guid)),
+        _createApp1ExtendedXmp(guid, xml.Length, split, second),
+        _createApp1ExtendedXmp(guid, xml.Length, 0, first),
+        _createSof(640, 480)));
+
+    using var jpeg = new JpegFile(stream);
+
+    Assert.AreEqual(comment, jpeg.Xmp.GetComment());
+  }
+
+  [TestMethod]
+  public void JpegFile_IgnoresExtendedXmpWithDifferentGuid() {
+    const string guid = "0123456789ABCDEF0123456789ABCDEF";
+    const string otherGuid = "FEDCBA9876543210FEDCBA9876543210";
+
+    var extendedXml = Encoding.UTF8.GetBytes(_createExtendedXmpXml("Wrong XMP"));
+
+    using var stream = new MemoryStream(
+      _createJpeg(
+        _createApp1Xmp(_createMainXmpWithExtendedGuid(guid)),
+        _createApp1ExtendedXmp(otherGuid, extendedXml.Length, 0, extendedXml),
+        _createSof(640, 480)));
+
+    using var jpeg = new JpegFile(stream);
+
+    // There is no usable extended XMP for our GUID.
+    // The main packet itself only contains HasExtendedXMP.
+    Assert.IsNull(jpeg.Xmp.GetComment());
+  }
+
+  [TestMethod]
+  public void JpegFile_MissingExtendedXmp_FallsBackToMainXmp() {
+    const string guid = "0123456789ABCDEF0123456789ABCDEF";
+
+    using var stream = new MemoryStream(
+      _createJpeg(
+        _createApp1Xmp(_createMainXmpWithExtendedGuid(guid)),
+        _createSof(640, 480)));
+
+    using var jpeg = new JpegFile(stream);
+
+    Assert.IsNull(jpeg.Xmp.GetComment());
+  }
+
+  [TestMethod]
+  public void JpegFile_InvalidSignature_Throws() {
+    using var stream = new MemoryStream([0x00, 0x00]);
+    Assert.ThrowsException<InvalidDataException>(() => new JpegFile(stream));
+  }
+
+  [TestMethod]
+  public void JpegFile_InvalidSegmentLength_Throws() {
+    using var stream = new MemoryStream([
+      0xFF, 0xD8,       // SOI
+      0xFF, 0xE0,       // APP0
+      0x00, 0x01,       // invalid length
+      0xFF, 0xDA        // SOS
+      ]);
+
+    using var jpeg = new JpegFile(stream);
+
+    Assert.ThrowsException<InvalidDataException>(() => jpeg.Exif);
+  }
+
+  [TestMethod]
+  public void JpegFile_TruncatedSegment_Throws() {
+    using var stream = new MemoryStream([
+      0xFF, 0xD8,       // SOI
+      0xFF, 0xE0,       // APP0
+      0x00, 0x20,       // says 32 bytes
+      0x01, 0x02, 0x03  // but only 3 bytes follow
+      ]);
+
+    using var jpeg = new JpegFile(stream);
+
+    Assert.ThrowsException<InvalidDataException>(() => jpeg.Exif);
+  }
+
+  [TestMethod]
+  public void JpegFile_InvalidSof_Throws() {
+    var sof = _createSegment(
+      0xC0,
+      [
+        8,              // precision
+        0, 10,          // height
+        0,              // truncated width
+      ]);
+
+    using var stream = new MemoryStream(_createJpeg(sof));
+    using var jpeg = new JpegFile(stream);
+
+    Assert.ThrowsException<InvalidDataException>(() => jpeg.Width);
+  }
 
   private static byte[] _createJpeg(params byte[][] segments) {
     using var stream = new MemoryStream();
@@ -294,7 +437,7 @@ public class JpegFileTests {
     var tiff = _createExifTiff(orientation);
     var payload = new byte[6 + tiff.Length];
 
-    _exifHeader.CopyTo(payload);
+    "Exif\0\0"u8.CopyTo(payload);
     tiff.CopyTo(payload, 6);
 
     return _createApp1(payload);
@@ -304,7 +447,7 @@ public class JpegFileTests {
     var xmlBytes = Encoding.UTF8.GetBytes(xml);
     var payload = new byte[29 + xmlBytes.Length];
 
-    _xmpHeader.CopyTo(payload);
+    "http://ns.adobe.com/xap/1.0/\0"u8.CopyTo(payload);
     xmlBytes.CopyTo(payload, 29);
 
     return _createApp1(payload);
@@ -361,4 +504,73 @@ public class JpegFileTests {
 
     return _createSegment(marker, payload);
   }
+
+  private static byte[] _createApp1ExtendedXmp(string guid, int fullLength, int offset, byte[] data) {
+    if (guid.Length != 32)
+      throw new ArgumentException("Extended XMP GUID must contain 32 characters.", nameof(guid));
+
+    var header = "http://ns.adobe.com/xmp/extension/\0"u8;
+    var guidBytes = Encoding.ASCII.GetBytes(guid);
+
+    const int guidLength = 32;
+    const int fullLengthSize = 4;
+    const int offsetSize = 4;
+
+    int payloadLength =
+      header.Length +
+      guidLength +
+      fullLengthSize +
+      offsetSize +
+      data.Length;
+
+    var payload = new byte[payloadLength];
+
+    int p = 0;
+
+    header.CopyTo(payload);
+    p += header.Length;
+
+    guidBytes.CopyTo(payload, p);
+    p += guidLength;
+
+    payload[p++] = (byte)(fullLength >> 24);
+    payload[p++] = (byte)(fullLength >> 16);
+    payload[p++] = (byte)(fullLength >> 8);
+    payload[p++] = (byte)fullLength;
+
+    payload[p++] = (byte)(offset >> 24);
+    payload[p++] = (byte)(offset >> 16);
+    payload[p++] = (byte)(offset >> 8);
+    payload[p++] = (byte)offset;
+
+    data.CopyTo(payload, p);
+
+    return _createApp1(payload);
+  }
+
+  private static string _createExtendedXmpXml(string comment) =>
+    $"""
+      <x:xmpmeta xmlns:x="adobe:ns:meta/">
+        <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                 xmlns:dc="http://purl.org/dc/elements/1.1/">
+          <rdf:Description>
+            <dc:description>
+              <rdf:Alt>
+                <rdf:li xml:lang="x-default">{comment}</rdf:li>
+              </rdf:Alt>
+            </dc:description>
+          </rdf:Description>
+        </rdf:RDF>
+      </x:xmpmeta>
+      """;
+
+  private static string _createMainXmpWithExtendedGuid(string guid) =>
+    $"""
+      <x:xmpmeta xmlns:x="adobe:ns:meta/">
+        <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                 xmlns:xmpNote="http://ns.adobe.com/xmp/note/">
+          <rdf:Description xmpNote:HasExtendedXMP="{guid}" />
+        </rdf:RDF>
+      </x:xmpmeta>
+      """;
 }

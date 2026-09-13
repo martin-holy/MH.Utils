72d056c013a058c699e9caffa78dd5395451fa90
martin-holy <martin.holy@hotmail.com>
2026-09-13 10:15:41 +0200

    [U] ExifOrientationExtensions

--- src/MH.Utils/Imaging/Exif/ExifOrientation.cs
+++ src/MH.Utils/Imaging/Exif/ExifOrientation.cs
@@ -12,13 +12,13 @@ public enum ExifOrientation : ushort {
 }
 
 public static class ExifOrientationExtensions {
-  // Orientation from "System.Photo.Orientation" have Rotate90 and Rotate270 swapped
+  // System.Photo.Orientation has Rotate90 and Rotate270 reversed compared to EXIF.
   public static Orientation? ToMsOrientation(this ExifOrientation? value) =>
     value switch {
       ExifOrientation.Normal => Orientation.Normal,
-      ExifOrientation.Rotate90 => Orientation.Rotate90,
+      ExifOrientation.Rotate90 => Orientation.Rotate270,
       ExifOrientation.Rotate180 => Orientation.Rotate180,
-      ExifOrientation.Rotate270 => Orientation.Rotate270,
+      ExifOrientation.Rotate270 => Orientation.Rotate90,
       ExifOrientation.FlipHorizontal => Orientation.FlipHorizontal,
       ExifOrientation.FlipVertical => Orientation.FlipVertical,
       ExifOrientation.Transpose => Orientation.Transpose,
@@ -26,13 +26,13 @@ public static class ExifOrientationExtensions {
       _ => null
     };
 
-  // Orientation from "System.Photo.Orientation" have Rotate90 and Rotate270 swapped
+  // System.Photo.Orientation has Rotate90 and Rotate270 reversed compared to EXIF.
   public static ExifOrientation? ToExifOrientation(this Orientation value) =>
     value switch {
       Orientation.Normal => ExifOrientation.Normal,
-      Orientation.Rotate90 => ExifOrientation.Rotate90,
+      Orientation.Rotate90 => ExifOrientation.Rotate270,
       Orientation.Rotate180 => ExifOrientation.Rotate180,
-      Orientation.Rotate270 => ExifOrientation.Rotate270,
+      Orientation.Rotate270 => ExifOrientation.Rotate90,
       Orientation.FlipHorizontal => ExifOrientation.FlipHorizontal,
       Orientation.FlipVertical => ExifOrientation.FlipVertical,
       Orientation.Transpose => ExifOrientation.Transpose,

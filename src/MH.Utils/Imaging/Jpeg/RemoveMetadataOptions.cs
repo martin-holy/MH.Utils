b418c2a3396bf915369b49a1e48f88468912aa63
martin-holy <martin.holy@hotmail.com>
2026-09-17 10:28:43 +0200

    [U] RemoveMetadataOptions: Thumbnail added

---
+++ src/MH.Utils/Imaging/Jpeg/RemoveMetadataOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MH.Utils.Imaging.Jpeg;
+
+[Flags]
+public enum RemoveMetadataOptions {
+  None = 0,
+  Exif = 1,
+  Xmp = 2,
+  Thumbnail = 4,
+  All = Exif | Xmp | Thumbnail
+}

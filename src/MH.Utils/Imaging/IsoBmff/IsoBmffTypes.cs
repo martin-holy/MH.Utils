4807cbf6d1868ff329a5531813ebd545b383b967
martin-holy <martin.holy@hotmail.com>
2026-09-18 19:06:50 +0200

    [N] IsoBmffTypes

---
+++ src/MH.Utils/Imaging/IsoBmff/IsoBmffTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace MH.Utils.Imaging.IsoBmff;
+
+internal static class IsoBmffTypes {
+  public const uint Ftyp = 0x66747970; // ftyp
+  public const uint Moov = 0x6D6F6F76; // moov
+  public const uint Mdat = 0x6D646174; // mdat
+  public const uint Trak = 0x7472616B; // trak
+  public const uint Mdia = 0x6D646961; // mdia
+  public const uint Hdlr = 0x68646C72; // hdlr
+  public const uint Vide = 0x76696465; // vide
+  public const uint Tkhd = 0x746B6864; // tkhd
+  public const uint Mdhd = 0x6D646864; // mdhd
+  public const uint Stbl = 0x7374626C; // stbl
+  public const uint Stts = 0x73747473; // stts
+}

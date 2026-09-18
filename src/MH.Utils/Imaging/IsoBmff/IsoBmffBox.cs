8127f63896113ed2ae8d5c79745c6acc0ecafcba
martin-holy <martin.holy@hotmail.com>
2026-09-18 19:07:25 +0200

    [N] IsoBmffBox

---
+++ src/MH.Utils/Imaging/IsoBmff/IsoBmffBox.cs
@@ -0,0 +1,11 @@
+﻿namespace MH.Utils.Imaging.IsoBmff;
+
+internal readonly struct IsoBmffBox(long offset, long headerSize, long size, uint type) {
+  public long Offset { get; } = offset;
+  public long HeaderSize { get; } = headerSize;
+  public long Size { get; } = size;
+  public uint Type { get; } = type;
+
+  public long DataOffset => Offset + HeaderSize;
+  public long DataSize => Size - HeaderSize;
+}

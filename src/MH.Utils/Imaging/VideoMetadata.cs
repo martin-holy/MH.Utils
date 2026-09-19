5e89b298394144038891c531fc46a148514d4803
martin-holy <martin.holy@hotmail.com>
2026-09-19 20:28:29 +0200

    [N] VideoMetadata

---
+++ src/MH.Utils/Imaging/VideoMetadata.cs
@@ -0,0 +1,23 @@
+﻿using MH.Utils.Imaging.IsoBmff;
+using System;
+using System.IO;
+
+namespace MH.Utils.Imaging;
+
+public sealed class VideoMetadata {
+  public int Width { get; }
+  public int Height { get; }
+  public int Orientation { get; }
+  public double? FrameRate { get; }
+  public TimeSpan? Duration { get; }
+
+  public VideoMetadata(Stream stream) {
+    var file = new IsoBmffFile(stream);
+
+    Width = file.Width;
+    Height = file.Height;
+    Orientation = file.Orientation;
+    FrameRate = file.FrameRate;
+    Duration = file.Duration;
+  }
+}

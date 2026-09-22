b9724fada0abc866e553cb6be5c18d70bf3c60d9
martin-holy <martin.holy@hotmail.com>
2026-09-22 18:08:39 +0200

    [N] IsoBmffMetadataEntry

---
+++ src/MH.Utils/Imaging/IsoBmff/IsoBmffMetadataEntry.cs
@@ -0,0 +1,6 @@
+﻿namespace MH.Utils.Imaging.IsoBmff;
+
+internal readonly struct IsoBmffMetadataEntry(string key, string value) {
+  public string Key { get; } = key;
+  public string Value { get; } = value;
+}

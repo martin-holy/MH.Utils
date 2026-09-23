9df6830f224ee80c774dd2b12b17af1ab220a9e6
martin-holy <martin.holy@hotmail.com>
2026-09-23 12:59:33 +0200

    [U] IsoBmffMetadataEntry

--- src/MH.Utils/Imaging/IsoBmff/IsoBmffMetadataEntry.cs
+++ src/MH.Utils/Imaging/IsoBmff/IsoBmffMetadataEntry.cs
@@ -1,6 +1,9 @@
 ﻿namespace MH.Utils.Imaging.IsoBmff;
 
-internal readonly struct IsoBmffMetadataEntry(string key, string value) {
+internal sealed class IsoBmffMetadataEntry(string key, string value, IsoBmffBox item, IsoBmffBox data) {
   public string Key { get; } = key;
   public string Value { get; } = value;
+
+  internal IsoBmffBox Item { get; } = item;
+  internal IsoBmffBox Data { get; } = data;
 }
